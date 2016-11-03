63d46856687b01a35386dd2c5d8edb88b535168a
Adina Stoica <adina.stoica@gmail.com>
2016-11-02 22:55:44 -0500

    Is List Palindrome almost complete - 2/3 methods

---
+++ CrackingTheCodingInterview/src/c02_LinkedLists/P6_Palindrome.java
@@ -0,0 +1,104 @@
+package c02_LinkedLists;
+
+import java.util.Stack;
+
+/**
+ * Implement a function to check if a
+ * 
+ * @author adina
+ *
+ */
+public class P6_Palindrome {
+	/**
+	 * First reverse the list and then make sure the two lists are equal
+	 * Note: comparison can only go to half the list, but not worth computing
+	 * size
+	 * 
+	 * @param list
+	 * @return whether the list and the reversed list are equal, thus a
+	 *         palindrome
+	 */
+	public static boolean isPalindromeReverseAndCompare(LinkedList<Integer> list) {
+		// empty list or one-element list is a palindrome
+		if (list == null || list.isEmpty() || list.head.next == null)
+			return true;
+		// reverse the linked list
+		LinkedList<Integer> reversed = reverse(list);
+		// compare the two lists
+		return isEqual(list, reversed);
+	}
+
+	/**
+	 * Is l1 equal to l2, element-wise?
+	 * 
+	 * @param l1
+	 * @param l2
+	 * @return if the two lists are equal in content
+	 */
+	private static boolean isEqual(LinkedList<Integer> l1, LinkedList<Integer> l2) {
+		Node<Integer> n1 = l1.head;
+		Node<Integer> n2 = l2.head;
+		while (n1 != null && n2 != null) {
+			if (n1.data != n2.data)
+				return false;
+			n1 = n1.next;
+			n2 = n2.next;
+		}
+		if (n1 != null || n2 != null)
+			return false;
+		return true;
+	}
+
+	/**
+	 * Reverse the contents of list, and return the reversed list
+	 * 
+	 * @param list the list to be reversed
+	 * @return the reversed list
+	 */
+	private static LinkedList<Integer> reverse(LinkedList<Integer> list) {
+		Node<Integer> n1 = list.head;
+		LinkedList<Integer> reversed = new LinkedList<>(n1.data);
+		while (n1.next != null) {
+			n1 = n1.next;
+			reversed.addFirst(n1.data);
+		}
+		return reversed;
+	}
+
+	/**
+	 * Use fast pointer and slow pointer to get half of class, and add value
+	 * slow pointer points at to a stack. Then pop from the stack comparing with
+	 * remainder of list.
+	 * 
+	 * @param list
+	 * @return is the list a palindrome
+	 */
+	public static boolean isPalindromeReverseFirstHalfStack(LinkedList<Integer> list) {
+		// empty list or one-element list is a palindrome
+		if (list == null || list.isEmpty() || list.head.next == null)
+			return true;
+		Node<Integer> fast = list.head.next;
+		Node<Integer> slow = list.head;
+
+		// add to stack until fast reaches end of list
+		Stack<Integer> stack = new Stack<>();
+		stack.add(slow.data);
+		while (fast != null) {
+			slow = slow.next;
+			fast = fast.next;
+			// don't double add the data
+			if (fast != null) {
+				stack.add(slow.data);
+				fast = fast.next;
+			}
+		}
+		// check if first half matches second half
+		while (slow != null) {
+			if (slow.data != stack.pop())
+				return false;
+			slow = slow.next;
+		}
+
+		return true;
+	}
+}
